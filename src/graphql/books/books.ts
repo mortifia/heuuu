e01221addce653eaf577ede0c47fb24e628497b6
mortifia <amela57290@gmail.com>
2021-11-11 11:05:15 +0100

    serialiseur v0 (it's alive)

--- src/graphql/books/books.ts
+++ src/graphql/books/books.ts
@@ -1,11 +1,41 @@
-"use strict";
-import { IResolvers } from "mercurius";
+'use strict'
+import { GraphQLResolveInfo } from 'graphql'
+import { IResolvers } from 'mercurius'
+import postgres from 'postgres'
+
+// generate sql with sheme for build sql and info of actual resolver to match exatly to the request
+function gqlToSql(schemeSql: typeof book, infoGql: GraphQLResolveInfo) {
+  const field = infoGql.fieldNodes[0].selectionSet?.selections.map(info => {
+    return info.name.value
+  })
+  // add the sql type (select | remove | ect ect)
+  let sql = `${schemeSql._type}`
+  // add field requested to sql
+  field?.forEach((fieldRequired: String | Object, pos) => {
+    if (typeof fieldRequired === 'string') {
+      sql += `${pos === 0 ? '' : ','} ${schemeSql[fieldRequired]}`
+    }
+  })
+  // add table to sql
+  sql += ` FROM ${schemeSql._}`
+  return sql
+}
+
+const book = {
+  _: 'book.book',
+  _type: 'SELECT',
+  id: 'book_id as id',
+  title: 'title',
+  author: 'author',
+}
 
 export const resolvers: IResolvers = {
   Query: {
     // books: () => [{ id: 1, title: "loool", author: "roibert" }],
-    books: async (_parent, args, ctx) => {
-      return ctx;
+    books: async (parent, args, ctx, info) => {
+      console.log(gqlToSql(book, info))
+      const tmp = await ctx.sql.unsafe(gqlToSql(book, info))
+      return tmp
     },
   },
-};
+}
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,76 @@
-"use strict";
-console.log("\nstart\n");
-const serviceName = "service-add"; //tracing
-import { tracer } from "./tracer.js"; //tracing
-tracer(serviceName); //tracing
+'use strict'
+console.log('\nstart\n')
+const serviceName = 'service-add' //tracing
+import { tracer } from './tracer.js' //tracing
+tracer(serviceName) //tracing
+import Fastify, {
+  FastifyInstance,
+  FastifyRegisterOptions,
+  FastifyReply,
+  FastifyRequest,
+} from 'fastify'
+import { buildSchema } from 'graphql'
 
-import Fastify, { FastifyInstance, FastifyRegisterOptions } from "fastify";
-import { buildSchema } from "graphql";
+import postgres from 'postgres'
+const sql = postgres('postgres://heuuu:heuuu@127.0.0.1:5432/heuuu', {
+  max: 10,
+})
 
-import glob from "tiny-glob";
-
+import glob from 'tiny-glob'
 import mercurius, {
   IResolvers,
   MercuriusOptions,
   MercuriusSchemaOptions,
-} from "mercurius";
-import mercuriusCodegen, { loadSchemaFiles, gql } from "mercurius-codegen";
-import lodash from "lodash";
-const merge = lodash.merge;
+} from 'mercurius'
+import mercuriusCodegen, { loadSchemaFiles, gql } from 'mercurius-codegen'
 
-import opentelemetry from "@autotelic/fastify-opentelemetry"; //tracing
+import lodash from 'lodash'
+const merge = lodash.merge
 
-const server: FastifyInstance = Fastify({});
+import opentelemetry from '@autotelic/fastify-opentelemetry' //tracing
+
+const server: FastifyInstance = Fastify({})
 
 const resolvers = await (async () => {
-  let resolvers = {};
-  const path = await glob("./build/graphql/**/*{.js}", { absolute: true });
+  let resolvers = {}
+  const path = await glob('./build/graphql/**/*{.js}', { absolute: true })
   for (let index = 0; index < path.length; index++) {
-    resolvers = merge(resolvers, (await import(path[index])).resolvers);
-    console.log(`lib loaded:  ${path[index]}`);
+    resolvers = merge(resolvers, (await import(path[index])).resolvers)
+    console.log(`lib loaded:  ${path[index]}`)
   }
-  console.log("end loading resolver\n");
-  return resolvers;
-})();
+  console.log('end loading resolver\n')
+  return resolvers
+})()
 
 /*
   merge schemas and build typescript definition
   with watcher for dev only */
-const { schema } = loadSchemaFiles("src/graphql/**/*.gql", {
+const { schema } = loadSchemaFiles('src/graphql/**/*.gql', {
   watchOptions: {
-    enabled: process.env.NODE_ENV === "development",
+    enabled: process.env.NODE_ENV === 'development',
     onChange(schema) {
-      server.graphql.replaceSchema(buildSchema(schema.join("\n")));
-      server.graphql.defineResolvers(resolvers);
+      server.graphql.replaceSchema(buildSchema(schema.join('\n')))
+      server.graphql.defineResolvers(resolvers)
       mercuriusCodegen(server, {
-        targetPath: "./src/graphql/generated.ts",
-      }).catch(console.error);
+        targetPath: './src/graphql/generated.ts',
+      }).catch(console.error)
     },
   },
-});
+})
+
+const buildContext = async (req: FastifyRequest, _reply: FastifyReply) => {
+  return {
+    authorization: req.headers.authorization,
+    sql: sql,
+  }
+}
+
+type PromiseType<T> = T extends PromiseLike<infer U> ? U : T
+
+declare module 'mercurius' {
+  interface MercuriusContext
+    extends PromiseType<ReturnType<typeof buildContext>> {}
+}
 
 /* options for graphql service */
 const mercuriusOpt: FastifyRegisterOptions<MercuriusOptions> = {
@@ -55,26 +78,27 @@ const mercuriusOpt: FastifyRegisterOptions<MercuriusOptions> = {
   resolvers: resolvers,
   graphiql: true,
   jit: 3,
-  path: "/",
-};
+  path: '/',
+  context: buildContext,
+}
 
 const start = async () => {
   try {
     // @ts-ignore
-    server.register(opentelemetry, { serviceName });
-    server.register(mercurius, mercuriusOpt);
-    await server.listen(3000);
-    const address = server.server.address();
-    typeof address === "string"
+    server.register(opentelemetry, { serviceName })
+    server.register(mercurius, mercuriusOpt)
+    await server.listen(3000)
+    const address = server.server.address()
+    typeof address === 'string'
       ? console.log(`🟩 run in ${address}${mercuriusOpt.path}`)
       : console.log(
           `🟩 run in http://${address?.address}:${address?.port}${mercuriusOpt.path}`
-        );
+        )
   } catch (err) {
-    console.error(`🟥 error in launch`);
-    console.error(err);
-    server.log.error(err);
-    process.exit(1);
+    console.error(`🟥 error in launch`)
+    console.error(err)
+    server.log.error(err)
+    process.exit(1)
   }
-};
-start();
+}
+start()
